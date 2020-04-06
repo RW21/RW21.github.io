c2490d4ccb05b27cda026a1f62f3ffd276064c07
RW <rinze.watanabe.translation@gmail.com>
2020-04-06 14:03:11 +1000

    Refresh

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// import baffle from 'buffle.js';
+const characters = '▓▒░█▓/ '
+const b = window.baffle('.baffle').set({
+    speed: 100
+});
+const c = window.baffle('.baffleInit').start().reveal(1500);
+
+
+b.start().set({
+    characters: '~▓▒░█▓æ–‡å­—åŒ–ã‘譁�ｭ怜喧縺�'
+});
